ed86ac9a5aea876fd7f30f625c8d212f62a8e4eb
Le Anh Vu <31981595+zz2zz22@users.noreply.github.com>
2021-05-04 19:51:08 +0700

    update main screen + tempData

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+import colors from './Colors'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider}/>
+          <Text style={styles.title}>
+            Todo <Text style={{fontWeight: "normal", color: colors.blue}}>Lists</Text>
+          </Text>
+          <View style={styles.divider}/>
+        </View>
+
+        <View style={{marginVertical: 48}}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={colors.blue}/>
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +35,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "bold",
+    color: colors.black,
+    paddingHorizontal: 64
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "bold",
+    fontSize: 14,
+    marginTop: 8
+  }
 });
---
+++ tempData.js
@@ -0,0 +1,18 @@
+//Dữ liệu tạm cho đến lúc kết nối với Firebase
+
+export default tempData = [
+    {
+        name: "Plan a trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book a flight",
+                completed: false
+            },
+            {
+                title: "Passport check",
+                completed: false
+            }
+        ]
+    }
+]
